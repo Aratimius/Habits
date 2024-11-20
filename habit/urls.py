48e579de2d1ff8c8603690a7ddb474d31e14f99e
Aratimius <aratimius@gmail.com>
2024-11-20 06:50:06 +0300

    DRF - рабочий проект

---
+++ habit/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from habit.apps import HabitConfig
+from habit.views import (
+    HabitListAPIView,
+    HabitCreateAPIView,
+    HabitUpdateAPIView,
+    HabitRetrieveAPIView,
+    HabitDestroyAPIView,
+    HabitPublishedListAPIView,
+)
+
+app_name = HabitConfig.name
+
+urlpatterns = [
+    path("", HabitListAPIView.as_view(), name="list"),
+    path("published/", HabitPublishedListAPIView.as_view(), name="published"),
+    path("create/", HabitCreateAPIView.as_view(), name="create"),
+    path("update/<int:pk>/", HabitUpdateAPIView.as_view(), name="update"),
+    path("retrieve/<int:pk>/", HabitRetrieveAPIView.as_view(), name="retrieve"),
+    path("destroy/<int:pk>/", HabitDestroyAPIView.as_view(), name="destroy"),
+]
